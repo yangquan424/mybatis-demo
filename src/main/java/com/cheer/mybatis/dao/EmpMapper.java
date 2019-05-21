d303a698ec190dfc9e0efbf6743e648a0963c7eb
yangquan <810946859@qq.com>
2019-05-21 14:58:50 +0800

    20190521

--- src/main/java/com/cheer/mybatis/dao/EmpMapper.java
+++ src/main/java/com/cheer/mybatis/dao/EmpMapper.java
@@ -2,6 +2,40 @@ package com.cheer.mybatis.dao;
 
 import com.cheer.mybatis.model.Emp;
 
+import java.util.List;
+
 public interface EmpMapper {
+    /**
+     * 查询一条数据
+     * @param empno 被查询的主键
+     * @return 返回被查询的数据
+     */
     Emp getEmp(Integer empno);
+
+    /**
+     * 插入一条数据
+     * @param emp 被插入的数据
+     * @return 返回被影响的行数
+     */
+    int insert(Emp emp);
+
+    /**
+     * 查询整个表的数据
+     * @return 返回整个表的数据
+     */
+    List<Emp> getEmpAll();
+
+    /**
+     * 删除一条数据
+     * @param emp 被删除的数据主键
+     * @return 返回被影响的行数
+     */
+    int doDelete(Emp emp);
+
+    /**
+     * 修改一条数据
+     * @param emp 被修改的数据主键
+     * @return 返回被影响的行数
+     */
+    int doUpdate(Emp emp);
 }
--- src/test/java/EmpMapperTest.java
+++ src/test/java/EmpMapperTest.java
@@ -1,3 +1,4 @@
+import com.cheer.mybatis.Util.MybatisUtils;
 import com.cheer.mybatis.dao.EmpMapper;
 import com.cheer.mybatis.model.Emp;
 import org.apache.ibatis.io.Resources;
@@ -8,10 +9,28 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
 
 public class EmpMapperTest {
     @Test
-    public void getEmp(){
+    public void getEmp() {
+        SqlSession session = MybatisUtils.getSession();
+
+        EmpMapper empMapper = session.getMapper(EmpMapper.class);
+
+        System.out.println(empMapper.getEmp(7788)  );
+
+        MybatisUtils.close(session);
+    }
+
+    @Test
+    public void insert() {
         String resource = "mybatis_config.xml";
         InputStream inputStream = null;
         try {
@@ -21,20 +40,112 @@ public class EmpMapperTest {
             SqlSession session = sqlSessionFactory.openSession();
 
             EmpMapper empMapper = session.getMapper(EmpMapper.class);
+            Emp emp =new Emp();
+            emp.setEmpno(1113);
+            emp.setEname("yash");
+            emp.setDeptno(20);
 
-            Emp emp = empMapper.getEmp(7788);
-
-            System.out.println(emp);
+            System.out.println(empMapper.insert(emp));
+            session.commit();
+            session.close();
 
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
                 inputStream.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+    @Test
+    public void getEmpAll(){
+        String resource = "mybatis_config.xml";
+        InputStream inputStream = null;
 
+        try {
+            inputStream = Resources.getResourceAsStream(resource);
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+            SqlSession session = sqlSessionFactory.openSession();
+
+            EmpMapper empMapper = session.getMapper(EmpMapper.class);
+
+            List<Emp> list = empMapper.getEmpAll();
+            for(Emp l:list){
+                System.out.println(l);
+            }
+            session.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    @Test
+    public void doDelete(){
+        String resource = "mybatis_config.xml";
+        InputStream inputStream = null;
+        try {
+            //导入 mybatis的配置文件,地址“mybatis_config.xml”
+            inputStream = Resources.getResourceAsStream(resource);
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+            SqlSession session = sqlSessionFactory.openSession();
+
+            EmpMapper empMapper = session.getMapper(EmpMapper.class);
+
+            Emp emp = new Emp();
+            emp.setEmpno(1111);
+
+            System.out.println(empMapper.doDelete(emp));
+            session.commit();;
+            session.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    @Test
+    public void doUpdate(){
+        String resource = "mybatis_config.xml";
+        InputStream inputStream = null;
+        try {
+            inputStream = Resources.getResourceAsStream(resource);
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+            SqlSession session = sqlSessionFactory.openSession();
+
+            EmpMapper empMapper = session.getMapper(EmpMapper.class);
+            Emp emp =new Emp();
+            emp.setEmpno(1211);
+            emp.setEname("yash");
+            emp.setDeptno(20);
+
+            System.out.println(empMapper.doUpdate(emp));
+            session.commit();
+            session.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
